41c052c2c59d8c582038d477c85e31f0735aba54
Byggvir <thomas@arend-rhb.de>
2018-05-27 20:35:07 +0200

    Old installation scrip deleted; on-up and on-down modified.

--- etc/fastd/ffrhb/on-down.sh
+++ etc/fastd/ffrhb/on-down.sh
@@ -3,7 +3,8 @@
 # Execute when fastd is going down
 # Autor: Thomas Arend
 # (c) 2018 by Thomas Arend
-# Date: 26.05.2018
+# Date: 27.05.2018
 
 ip link delete br-client type bridge
 ip link set down dev bat0
+ip link delete dev bat0
--- etc/fastd/ffrhb/on-up.sh
+++ etc/fastd/ffrhb/on-up.sh
@@ -3,22 +3,23 @@
 # Execute when fastd is going up
 # Autor: Thomas Arend
 # (c) 2017 by Thomas Arend
-# Date: 26.05.2018
+# Date: 27.05.2018
 
 INTERFACE=${1:-mesh-vpn}
+logger "fastd on-up started"
 
 # Start meshing on fastd tunnel
-ip link set up dev $INTERFACE || echo Error: Interface $INTERFACE not set up
+ip link set up dev $INTERFACE || logger "Error: Interface $INTERFACE not set up"
 
 # Add tunnel interface to mesh
-batctl if add $INTERFACE || echo Error: Interface $INTERFACE not added to batman
+batctl if add $INTERFACE || logger "Error: Interface $INTERFACE not added to batman"
 #sysctl net.ipv6.conf.bat0.disable_ipv6=1
 #sysctl net.ipv6.conf.mesh-vpn.disable_ipv6=1
 
 # Set mtu for bat0
 # Change 1364 to your value in the site.conf for the FF firmware
 
-ip link set up dev bat0 mtu 1364 || echo Error when setting MTU to 1364
+ip link set up dev bat0 mtu 1364 || logger "Error when setting MTU to 1364"
 batctl gw client 45
 batctl orig_interval 5000
 batctl multicast_mode 0
@@ -32,6 +33,6 @@ ip link set bat0 master br-client
 #  Add ipv6 addr with scope global to bat0
 
 NIP='fda0:747e:ab29:2241'
-GIP=`ip addr show dev br-client | grep 'inet6 fe80::.* scope link' | sed 's#.*inet6 fe80::##; s# .*$##'`
+GIP=`ip addr show dev bat0 | grep 'inet6 fe80::.* scope link' | sed 's#.*inet6 fe80::##; s# .*$##'`
 
-ip addr add ${NIP}:${GIP} dev br-client scope global || echo Error: IPv6 ${NIP}:${GIP} not set
+ip addr add ${NIP}:${GIP} dev br-client scope global || logger "Error: IPv6 ${NIP}:${GIP} not set"
